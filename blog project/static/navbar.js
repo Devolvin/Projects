aa5361a02722a7577e6804cc961127d586660cdb
Devolvin <testin4python@gmail.com>
2022-09-23 13:39:20 +0300

    first commit, some html and css

---
+++ blog project/static/navbar.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+//$( ".navbar-toggle" ).click(function() {
+//  $( ".navbar" ).addClass( "activated" );
+//  $( ".page-header-heading" ).addClass( "red" );
+//});
+
+$('#main-nav').on('hidden.bs.collapse', function () {
+    $('#main-nav').removeClass("main-nav-toggled");
+      $('#main-nav').addClass("main-nav-untoggled");
+      $('#main-nav').removeClass("text-center");
+      $('.navbar-brand').addClass("mx-5");
+      $('.navbar-brand').removeClass("navbar-brand-toggled");
+      $('#nav-register').addClass("me-3");
+      $('#nav-login').addClass("me-5");
+});
+
+
+$('#main-nav').on('shown.bs.collapse', function () {
+      $('#main-nav').removeClass("main-nav-untoggled");
+      $('#main-nav').addClass("main-nav-toggled");
+      $('#main-nav').addClass("text-center");
+      $('.navbar-brand').removeClass("mx-5");
+      $('.navbar-brand').addClass("navbar-brand-toggled");
+      $('#nav-register').removeClass("me-3");
+      $('#nav-login').removeClass("me-5");
+
+});
+
+//$( ".button1" ).click(function() {
+//  $(".page-header-heading").addClass( "red" );
+//});
+});
