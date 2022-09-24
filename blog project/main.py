0e6d11c4cbc311db2c5e7bae8a217e2d99cb7dbc
Devolvin <testin4python@gmail.com>
2022-09-24 11:36:15 +0300

    implement register and login

--- blog project/main.py
+++ blog project/main.py
@@ -1,11 +1,109 @@
 from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager
+from flask_bootstrap import Bootstrap
+from forms import RegisterForm , LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'q#f12]4b\J5VVc419F2F]pU'
+Bootstrap(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Configure tables
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="author")
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    # This is for choosing who can post, edit posts, delete posts and stuff
+    # Currently on 3 levels
+    admin_level = db.Column(db.Integer, nullable=False, default=1)
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    # Create Foreign Key, "users.id" the users refers to the tablename of User.
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Create reference to the User object, the "posts" refers to the posts property in the User class.
+    author = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="comments")
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    post = relationship("BlogPost", back_populates="comments")
+    comment = db.Column(db.Integer, nullable=False)
+
+
+db.create_all()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.username.data
+        if User.query.filter_by(email=email).first():
+            return redirect(url_for("index"))
+        if User.query.filter_by(username=username).first():
+            return redirect(url_for("index"))
+        hashed_pw = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(email=email,
+                        username= username,
+                        password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("index"))
+
+    return render_template("register.html", form=form)
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            return redirect(url_for('login'))
+        if not check_password_hash(user.password, form.password.data):
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for("index"))
+    return render_template("login.html",form=form)
+
 
 
 if __name__ == "__main__":
